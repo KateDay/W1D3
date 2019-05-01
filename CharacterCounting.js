24cc46f933590d86ed2d3778df64f99a4be9f30a
Kate Day <48929128+KateDay@users.noreply.github.com>
2019-05-01 17:31:18 -0600

    Update CharacterCounting.js

--- CharacterCounting.js
+++ CharacterCounting.js
@@ -1,6 +1,19 @@
-var countLetters = Function(){
+function countLetters(given) {
+
+  var unique = given.split(" ").join("");
+  var letterCount = {};
+
+  for (var i = 0; i < unique.length; i++) {
+    var char = unique[i];
+    if (letterCount[char] === undefined) {
+      letterCount[char] = 1;
+    } else {
+      letterCount[char]++ ;
+    }
 
 }
+return letterCount;
+}
 
-
-console.log("lighthouse in the house");
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("hummitzsch"));
